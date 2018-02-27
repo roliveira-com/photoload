1d554b52387de92cf1e739d9ee19eb9e6b8a65b9
Rodrigo Oliveira <rodrigooliveira@rodrigo.local>
2018-02-27 08:18:36 -0300

    implementando cards

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,34 +1,24 @@
-import { normalize } from "path";
 
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
-  // ***
-  // Como segundo parâmetro, é possivel definir o escopo em que o service worker atuará
-  // sobreescrevendo o que foi definido no manifest.json. Abaixo, o escopo atuará apenas
-  // na pasta /help
-  // ***
-  // navigator.serviceWorker.register('/sw.js', {scope: '/help/'})
-  navigator.serviceWorker.register('/sw.js')
-    .then(function(){
-      console.log('o service worker foi registrado')
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-// ***
-// Evitando a mensagem de instalação no lifecycle normal
-// A mensagem perguntando se o usuário deseja instalar o app é exibida automaticamente pelo 
-// chrome sempre que o usuário visitar o aiste pelo menos duas vezes em um intervalo de 5
-// minutos. Contudo podemos configurar para que este mesnagem apareça em outro momento. Para isto, 
-// capturamos o evento 'beforeinstallprompt' e guardamos na variável 'deferredPrompt' conforme abaixo.
-// No Final, retornamos fase para que nada aconteça para o usuário naquele momento. Neste caso, a
-// mensagem de instalação deve aparecer quando o usuário for incluir uma nova foto. Para isto, configuramos 
-// o evento no script do botão no arquivo feed.js
-// ***
-window.addEventListener('beforeinstallprompt', function(event){
-  console.log('evento beforeinstallprompt disparado');
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
-  console.log(deferredPrompt)
   return false;
-})
+});
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,11 @@
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Instalando service worker...', event);
+
+self.addEventListener('install', function(event) {
 });
 
-self.addEventListener('activate', function(event){
-  console.log('[Service Worker] Ativando o service worker...', event);
+self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-  console.log('[Service Worker] fazendo uma requisição', event)
+self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
-})
+});
