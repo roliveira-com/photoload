99123e33224b93a006e91c901aaea83ca7a18c3c
Rodrigo Oliveira <rodrigooliveira@rodrigo.home>
2018-03-20 08:32:53 -0300

    configs do cache e cache dinamico com workbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,23 @@
 importScripts('workbox-sw.prod.v2.1.3.js');
 
 const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'google-fonts',
+  cacheExpiration: {
+    maxEntries: 5,
+    maxAgeSeconds:60 * 60 * 24 * 30
+  }
+}));
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'material-css'
+}));
+
+workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'post-images'
+}));
+
 workboxSW.precache([
   {
     "url": "404.html",
@@ -21,6 +38,10 @@ workboxSW.precache([
   {
     "url": "offline.html",
     "revision": "9fdc638fd9eebf0a912240529c66df70"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "f76fbb40724abeb165ad7cc036760ef6"
   },
   {
     "url": "src/css/app.css",
@@ -68,7 +89,7 @@ workboxSW.precache([
   },
   {
     "url": "sw-base.js",
-    "revision": "0304aa4792e06e0a839100d66b2853ea"
+    "revision": "2a2639b0e80c5acf7230f7d66a02fd7b"
   },
   {
     "url": "sw.js",
--- public/sw-base.js
+++ public/sw-base.js
@@ -1,4 +1,21 @@
 importScripts('workbox-sw.prod.v2.1.3.js');
 
 const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'google-fonts',
+  cacheExpiration: {
+    maxEntries: 5,
+    maxAgeSeconds:60 * 60 * 24 * 30
+  }
+}));
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'material-css'
+}));
+
+workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'post-images'
+}));
+
 workboxSW.precache([]);
